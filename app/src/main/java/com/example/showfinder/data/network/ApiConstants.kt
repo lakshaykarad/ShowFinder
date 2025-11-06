ad08a1fda876a6c1f73373bf6700d5a4d5e8002f
Lakshay <164053950+lakshaykarad@users.noreply.github.com>
2025-11-06 21:35:10 +0530

    Update ApiConstants.kt

--- app/src/main/java/com/example/showfinder/data/network/ApiConstants.kt
+++ app/src/main/java/com/example/showfinder/data/network/ApiConstants.kt
@@ -1,9 +1,9 @@
 package com.example.showfinder.data.model.network
 
 object ApiConstants {
-    // old api = ajMsetaDt6Y8XvIksgDgNtNSH3U8WDWKkDyZ4lew
+     
     const val BASE_URL = "https://api.watchmode.com/v1/"
-    const val API_KEY = "ajMsetaDt6Y8XvIksgDgNtNSH3U8WDWKkDyZ4lew"
+    const val API_KEY = "Enter Your Apis"
     const val OMDB_BASE_URL = "https://www.omdbapi.com/"
-    const val OMDb_API_KEY = "f645d2d4"
+    const val OMDb_API_KEY = "Enter Your Apis"
 }
